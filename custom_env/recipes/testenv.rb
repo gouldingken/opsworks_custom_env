7ac6791820b8c93a6aefd593280e0b076fe5c4b9
gouldingken <gouldingken@gmail.com>
2013-03-14 17:42:47 -0400

    check deploy dir

--- custom_env/recipes/testenv.rb
+++ custom_env/recipes/testenv.rb
@@ -3,7 +3,7 @@
 
 Chef::Log.info("Listing Env Variables")
 
-command "export TEST_VAR=helloNode"
+#command "export TEST_VAR=helloNode"
 Chef::Log.info("To go in #{@deploy[:deploy_to]}/shared/config/vars.json")
 
 
