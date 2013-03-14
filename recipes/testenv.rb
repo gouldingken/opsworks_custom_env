5dc07d7fc4d95538e2a79dc2b83d2730f5b36f97
gouldingken <gouldingken@gmail.com>
2013-03-14 13:44:58 -0400

    Chef::Log.info

--- recipes/testenv.rb
+++ recipes/testenv.rb
@@ -2,6 +2,6 @@
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
 rightscale_marker :begin
-log "Hello World! You should try Chef with RightScale!"
-log "my_app: #{node[:custom_env][:my_app]}"
+Chef::Log.info( "Hello World! You should try Chef with RightScale!")
+Chef::Log.info("my_app: #{node[:custom_env][:my_app]}")
 rightscale_marker :end
