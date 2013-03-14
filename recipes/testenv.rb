b28049361ec30987406c4e9765879a751edd58fc
gouldingken <gouldingken@gmail.com>
2013-03-14 14:31:46 -0400

    back to custom_env

--- recipes/testenv.rb
+++ recipes/testenv.rb
@@ -1,5 +1,5 @@
 # Set up app's custom configuration in the environment.
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
-Chef::Log.info("Hello World! You should try Chef with RightScale!")
-Chef::Log.info("my_app: #{node[:custom_env][:my_app]}")
+Chef::Log.info("Hello World")
+#Chef::Log.info("my_app: #{node[:custom_env][:my_app]}")
