8461ab8fcc104087dc7a462c604fe18273b4ab0f
gouldingken <gouldingken@gmail.com>
2013-03-14 17:13:46 -0400

    check deploy dir

--- custom_env/recipes/testenv.rb
+++ custom_env/recipes/testenv.rb
@@ -1,7 +1,7 @@
 # Set up app's custom configuration in the environment.
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
-Chef::Log.info("Listing Env Variables")
+Chef::Log.info("Listing Env Variables to go in #{params[:deploy][:deploy_to]}/shared/config/vars.json")
 node[:custom_env].each do |application, vars|
   Chef::Log.info("application: #{application}")
   vars.each do |k, v|
